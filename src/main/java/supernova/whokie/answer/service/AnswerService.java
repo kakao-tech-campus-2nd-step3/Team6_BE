87399fa4a69aae060426b9db6f7d5fb4e6275c64
geonny <kky8586696@naver.com>
2024-09-15 21:19:19 +0900

    Feat: 랜덤 공통 질문 답하기 기능 메서드 추가

--- src/main/java/supernova/whokie/answer/service/AnswerService.java
+++ src/main/java/supernova/whokie/answer/service/AnswerService.java
@@ -9,6 +9,10 @@ import supernova.whokie.answer.Answer;
 import supernova.whokie.answer.controller.dto.AnswerResponse;
 import supernova.whokie.answer.repository.AnswerRepository;
 import supernova.whokie.global.dto.PagingResponse;
+import supernova.whokie.question.Question;
+import supernova.whokie.question.repository.QuestionRepository;
+import supernova.whokie.user.Users;
+import supernova.whokie.user.repository.UsersRepository;
 
 import java.util.List;
 
@@ -16,7 +20,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AnswerService {
 
-    private AnswerRepository answerRepository;
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
+    private final UsersRepository usersRepository;
 
     public PagingResponse<AnswerResponse.Record> getAnswerRecord(Pageable pageable){
         Page<Answer> answers = answerRepository.findAll(pageable);
@@ -26,6 +32,21 @@ public class AnswerService {
                 .toList();
 
         return PagingResponse.from(new PageImpl<>(answerResponse, pageable, answers.getTotalElements()));
+    }
+
+    public void answerToCommonQuestion(Users user, Long questionId, Long pickedId){
+        Question question = questionRepository.findById(questionId)
+                .orElse(null); //TODO 예외처리
+        Users picked = usersRepository.findById(pickedId)
+                .orElse(null); //TODO 예외처리
+
+        Answer answer = Answer.builder()
+                .question(question)
+                .picker(user)
+                .picked(picked)
+                .hintCount(0)
+                .build();
+        answerRepository.save(answer);
     }
 
 
