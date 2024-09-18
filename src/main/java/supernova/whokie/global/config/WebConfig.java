e44238a495b884be61a9c5dfc1d3fac77b0974a8
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-09-18 23:38:48 +0900

    Feat: 인터셉터를 통한 토큰 검증

---
+++ src/main/java/supernova/whokie/global/config/WebConfig.java
@@ -0,0 +1,34 @@
+package supernova.whokie.global.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import supernova.whokie.global.auth.JwtInterceptor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // Todo 경로 수정 해야함
+        registry.addInterceptor(jwtInterceptor)
+            .addPathPatterns("/api/user/mypage");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true)
+            .exposedHeaders("Authorization")
+            .maxAge(3600);
+    }
+}
