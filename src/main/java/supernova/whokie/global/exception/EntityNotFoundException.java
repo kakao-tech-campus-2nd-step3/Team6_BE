4bd645124ab0a29b08a5e8e6961960343edc80c9
hjinshin <gudwls818@gmail.com>
2024-09-22 18:18:29 +0900

    Feat: EntityNotFoundException 작성
    데이터가 존재하지 않을 때 발생하는 커스텀 오류

---
+++ src/main/java/supernova/whokie/global/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package supernova.whokie.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends CustomException {
+    private static final String DEFAULT_TITLE = "Date Not Found";
+
+    public EntityNotFoundException(String message) {
+        super(message, HttpStatus.NOT_FOUND, DEFAULT_TITLE);
+    }
+}
