db5ff4e9b7683e45dc5ab7c8a46d3cb9fef1dcd2
momnpa333 <momnpa333@gmail.com>
2024-09-24 16:18:13 +0900

    Feat: 프로필 질문 조회 api 제작

--- src/main/java/supernova/whokie/profile_question/controller/ProfileQuestionController.java
+++ src/main/java/supernova/whokie/profile_question/controller/ProfileQuestionController.java
@@ -1,35 +1,38 @@
 package supernova.whokie.profile_question.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import supernova.whokie.global.dto.GlobalResponse;
+import supernova.whokie.global.dto.PagingResponse;
 import supernova.whokie.profile_question.controller.dto.ProfileQuestionResponse;
 
-import java.awt.print.Pageable;
-import java.time.LocalDate;
-import java.util.List;
+import supernova.whokie.profile_question.service.ProfileQuestionService;
+import supernova.whokie.profile_question.service.dto.ProfileQuestionModel;
 
 @RestController
 @RequestMapping("/api/profile/question")
+@RequiredArgsConstructor
 public class ProfileQuestionController {
 
+    private final ProfileQuestionService profileQuestionService;
+
     @GetMapping("/{user-id}")
-    public ProfileQuestionResponse.Questions getProfileQuestions(
-            @PathVariable("user-id") String userId,
-            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
+    public PagingResponse<ProfileQuestionResponse.Question> getProfileQuestions(
+        @PathVariable("user-id") Long userId,
+        @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.ASC) Pageable pageable
     ) {
-        return ProfileQuestionResponse.Questions.builder()
-                .questions(
-                        List.of(
-                                new ProfileQuestionResponse.Question(1L, "quest1", LocalDate.now()),
-                                new ProfileQuestionResponse.Question(1L, "quest1", LocalDate.now())))
-                .build();
+        Page<ProfileQuestionModel.Info> page = profileQuestionService.getProfileQuestions(userId,
+            pageable);
+        return PagingResponse.from(page.map(ProfileQuestionResponse.Question::from));
     }
 
     @DeleteMapping("/{profile-question-id}")
     public GlobalResponse deleteProfileQuestion(
-            @PathVariable("profile-question-id") String profileQuestionId
+        @PathVariable("profile-question-id") String profileQuestionId
     ) {
         return GlobalResponse.builder().message("message").build();
     }
