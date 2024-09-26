f5c77ef9479c8780e0867b199ba7d159ac734afb
momnpa333 <momnpa333@gmail.com>
2024-09-26 15:47:09 +0900

    Feat: profileAnswer 제약 조건 추가

--- src/main/java/supernova/whokie/profile_answer/ProfileAnswer.java
+++ src/main/java/supernova/whokie/profile_answer/ProfileAnswer.java
@@ -1,6 +1,10 @@
 package supernova.whokie.profile_answer;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,16 +25,19 @@ import supernova.whokie.user.Users;
 public class ProfileAnswer extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String content;
 
-    @ManyToOne
-    @JoinColumn(name = "profile_question_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "profile_question_id", nullable = false)
     private ProfileQuestion profileQuestion;
 
-    @ManyToOne
-    @JoinColumn(name = "answered_user_id")
-    private Users answeredUsers;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "answered_user_id", nullable = false)
+    private Users answeredUser;
+
 
 }
