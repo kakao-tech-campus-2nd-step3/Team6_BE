8156008db3f7bfb497a2b7d876374186a9d68efc
momnpa333 <momnpa333@gmail.com>
2024-09-26 15:58:58 +0900

    Feat: 더미 api uri 수정

--- src/main/java/supernova/whokie/profile/controller/ProfileController.java
+++ src/main/java/supernova/whokie/profile/controller/ProfileController.java
@@ -13,15 +13,15 @@ public class ProfileController {
 
     @GetMapping("/{user-id}")
     public ProfileResponse.Info getProfileInfo(
-            @PathVariable("user-id") String userId
+        @PathVariable("user-id") Long userId
     ) {
         return ProfileResponse.Info.builder()
-                .totalVisited(1)
-                .totalVisited(12)
-                .description("홍길동의 프로필")
-                .backgroundImageUrl("backgroundImageUrl")
-                .name("홍길동")
-                .build();
+            .totalVisited(1)
+            .totalVisited(12)
+            .description("홍길동의 프로필")
+            .backgroundImageUrl("backgroundImageUrl")
+            .name("홍길동")
+            .build();
     }
 
     @GetMapping("/increase")
