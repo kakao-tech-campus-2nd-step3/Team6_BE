b2396574be503b5f83c75f11ff88adc2ab279e23
momnpa333 <momnpa333@gmail.com>
2024-09-26 17:30:37 +0900

    Feat: deleteAllByProfileQuestionId 제작

--- src/main/java/supernova/whokie/profile_answer/infrastructure/repository/ProfileAnswerRepository.java
+++ src/main/java/supernova/whokie/profile_answer/infrastructure/repository/ProfileAnswerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.profile_answer.ProfileAnswer;
 
 @Repository
@@ -16,4 +17,7 @@ public interface ProfileAnswerRepository extends JpaRepository<ProfileAnswer, Lo
 
     @Query("SELECT pa FROM ProfileAnswer pa JOIN FETCH pa.answeredUser WHERE pa.id = :id")
     Optional<ProfileAnswer> findByIdWithAnsweredUser(Long id);
+
+    @Transactional
+    void deleteAllByProfileQuestionId(Long profileQuestionId);
 }
