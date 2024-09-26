8e542e230c76a6c6d22d614eb9c75338c5607662
momnpa333 <momnpa333@gmail.com>
2024-09-26 15:58:39 +0900

    Feat: createProfileAnswer 제작

--- src/main/java/supernova/whokie/profile_answer/service/ProfileAnswerService.java
+++ src/main/java/supernova/whokie/profile_answer/service/ProfileAnswerService.java
@@ -4,19 +4,41 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import supernova.whokie.global.exception.EntityNotFoundException;
 import supernova.whokie.profile_answer.ProfileAnswer;
 import supernova.whokie.profile_answer.infrastructure.repository.ProfileAnswerRepository;
+import supernova.whokie.profile_answer.service.dto.ProfileAnswerCommand;
+import supernova.whokie.profile_answer.service.dto.ProfileAnswerCommand.Create;
 import supernova.whokie.profile_answer.service.dto.ProfileAnswerModel;
+import supernova.whokie.profile_question.ProfileQuestion;
+import supernova.whokie.profile_question.infrastructure.repository.ProfileQuestionRepository;
+import supernova.whokie.user.Users;
+import supernova.whokie.user.repository.UsersRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileAnswerService {
 
+    private final UsersRepository usersRepository;
+    private final ProfileQuestionRepository profileQuestionRepository;
     private final ProfileAnswerRepository profileAnswerRepository;
 
     public Page<ProfileAnswerModel.Info> getProfileAnswers(Long userId, Pageable pageable) {
         Page<ProfileAnswer> profileAnswers = profileAnswerRepository.findAllByUserId(userId,
             pageable);
         return profileAnswers.map(ProfileAnswerModel.Info::from);
+    }
+
+    public void createProfileAnswer(Long answeredUserId, ProfileAnswerCommand.Create command) {
+        Users answeredUser = usersRepository.findById(answeredUserId)
+            .orElseThrow(() -> new EntityNotFoundException("해당하는 사용자가 존재하지 않습니다."));
+
+        ProfileQuestion profileQuestion = profileQuestionRepository.findById(
+                command.profileQuestionId())
+            .orElseThrow(() -> new EntityNotFoundException("해당하는 프로필 질문이 존재하지 않습니다."));
+
+        ProfileAnswer profileAnswer = command.toEntity(answeredUser, profileQuestion);
+
+        profileAnswerRepository.save(profileAnswer);
     }
 }
