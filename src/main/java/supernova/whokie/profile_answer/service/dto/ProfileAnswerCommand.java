1d6f3229777034ac3aa01456bf416ba2408c88d0
momnpa333 <momnpa333@gmail.com>
2024-09-26 15:57:52 +0900

    Feat: toEntity 제작

--- src/main/java/supernova/whokie/profile_answer/service/dto/ProfileAnswerCommand.java
+++ src/main/java/supernova/whokie/profile_answer/service/dto/ProfileAnswerCommand.java
@@ -1,5 +1,29 @@
 package supernova.whokie.profile_answer.service.dto;
 
-public class ProfileAnswerCommand {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import supernova.whokie.global.exception.SelfValidating;
+import supernova.whokie.profile_answer.ProfileAnswer;
+import supernova.whokie.profile_question.ProfileQuestion;
+import supernova.whokie.user.Users;
 
+public class ProfileAnswerCommand extends SelfValidating {
+
+    @Builder
+    public record Create(
+        @NotNull
+        Long profileQuestionId,
+        @NotBlank
+        String content
+    ) {
+
+        public ProfileAnswer toEntity(Users answeredUser, ProfileQuestion profileQuestion) {
+            return ProfileAnswer.builder()
+                .content(content)
+                .profileQuestion(profileQuestion)
+                .answeredUser(answeredUser)
+                .build();
+        }
+    }
 }
