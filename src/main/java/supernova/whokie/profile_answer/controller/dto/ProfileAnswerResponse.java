da6b1815a07aea193ad96d4a2f8b6e93a410db30
momnpa333 <momnpa333@gmail.com>
2024-09-26 14:30:58 +0900

    Feat: Response에 정적 메서드 from 제작

--- src/main/java/supernova/whokie/profile_answer/controller/dto/ProfileAnswerResponse.java
+++ src/main/java/supernova/whokie/profile_answer/controller/dto/ProfileAnswerResponse.java
@@ -1,18 +1,29 @@
 package supernova.whokie.profile_answer.controller.dto;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 import java.time.LocalDate;
+import supernova.whokie.profile_answer.service.dto.ProfileAnswerModel;
+import supernova.whokie.profile_question.service.dto.ProfileQuestionModel;
 
 public class ProfileAnswerResponse {
 
     @Builder
     public record Answer(
-            Long profileAnswerId,
-            String content,
-            String profileQuestionContent,
-            LocalDate createdAt
+        Long profileAnswerId,
+        String content,
+        String profileQuestionContent,
+        LocalDateTime createdAt
     ) {
 
+        public static Answer from(ProfileAnswerModel.Info model) {
+            return new Answer(
+                model.profileAnswerId(),
+                model.content(),
+                model.profileQuestionInfo().content(),
+                model.createdAt()
+            );
+        }
     }
 }
