0cab31e9ede8e51cb5df2b2af5fbe1955e1c72ef
momnpa333 <momnpa333@gmail.com>
2024-09-26 17:34:58 +0900

    Feat: FindByIdWithUser 제작

--- src/main/java/supernova/whokie/profile_question/infrastructure/repository/ProfileQuestionRepository.java
+++ src/main/java/supernova/whokie/profile_question/infrastructure/repository/ProfileQuestionRepository.java
@@ -1,8 +1,10 @@
 package supernova.whokie.profile_question.infrastructure.repository;
 
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import supernova.whokie.profile_question.ProfileQuestion;
 
@@ -10,4 +12,7 @@ import supernova.whokie.profile_question.ProfileQuestion;
 public interface ProfileQuestionRepository extends JpaRepository<ProfileQuestion, Long> {
 
     Page<ProfileQuestion> findAllByUserId(Long userId, Pageable pageable);
+
+    @Query("SELECT pq FROM ProfileQuestion pq JOIN FETCH pq.user WHERE pq.id = :profileQuestionId")
+    Optional<ProfileQuestion> findByIdWithUser(Long profileQuestionId);
 }
