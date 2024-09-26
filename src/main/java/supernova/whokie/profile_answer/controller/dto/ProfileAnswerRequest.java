91efcac060758fcd40da110330e46e9914975518
momnpa333 <momnpa333@gmail.com>
2024-09-26 15:57:40 +0900

    Feat: ProfileAnswerRequest 에 toCommand 제작

--- src/main/java/supernova/whokie/profile_answer/controller/dto/ProfileAnswerRequest.java
+++ src/main/java/supernova/whokie/profile_answer/controller/dto/ProfileAnswerRequest.java
@@ -1,14 +1,23 @@
 package supernova.whokie.profile_answer.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import supernova.whokie.profile_answer.service.dto.ProfileAnswerCommand;
 
 public class ProfileAnswerRequest {
 
     @Builder
     public record Answer(
-            String content,
-            Long profileQuestionId
+        String content,
+        Long profileQuestionId
     ) {
 
+        public ProfileAnswerCommand.Create toCommand() {
+            return ProfileAnswerCommand.Create.builder()
+                .content(content())
+                .profileQuestionId(profileQuestionId())
+                .build();
+        }
     }
 }
