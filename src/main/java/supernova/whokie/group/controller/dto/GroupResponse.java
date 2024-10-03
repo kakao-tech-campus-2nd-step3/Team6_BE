e7581456009c3b598fe44f04a07306168d2efd13
momnpa333 <momnpa333@gmail.com>
2024-10-03 18:18:13 +0900

    Feat: InfoWithMemberCount 제작

--- src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
+++ src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
@@ -1,16 +1,34 @@
 package supernova.whokie.group.controller.dto;
 
 import lombok.Builder;
+import supernova.whokie.group.service.dto.GroupModel;
 
 public class GroupResponse {
 
     @Builder
     public record Info(
-            Long groupId,
-            String groupName,
-            String groupImageUrl,
-            int groupMemberCount
+        Long groupId,
+        String groupName,
+        String groupImageUrl
     ) {
 
+    }
+
+    @Builder
+    public record InfoWithMemberCount(
+        Long groupId,
+        String groupName,
+        String groupImageUrl,
+        Long groupMemberCount
+    ) {
+
+        public static InfoWithMemberCount from(GroupModel.InfoWithMemberCount infoWithMemberCount) {
+            return InfoWithMemberCount.builder()
+                .groupId(infoWithMemberCount.groupId())
+                .groupName(infoWithMemberCount.groupName())
+                .groupImageUrl(infoWithMemberCount.groupImageUrl())
+                .groupMemberCount(infoWithMemberCount.groupMemberCount())
+                .build();
+        }
     }
 }
