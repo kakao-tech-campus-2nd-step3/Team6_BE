c4efbf66589e11c3c48444f5f3efa7cc793e03e8
momnpa333 <momnpa333@gmail.com>
2024-10-03 18:18:24 +0900

    Feat: getGroups 제작

--- src/main/java/supernova/whokie/group/service/GroupService.java
+++ src/main/java/supernova/whokie/group/service/GroupService.java
@@ -2,11 +2,15 @@ package supernova.whokie.group.service;
 
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.group.Groups;
 import supernova.whokie.group.infrastructure.repository.GroupsRepository;
+import supernova.whokie.group.infrastructure.repository.dto.GroupInfoWithMemberCount;
 import supernova.whokie.group.service.dto.GroupCommand;
+import supernova.whokie.group.service.dto.GroupModel;
 
 @Service
 @RequiredArgsConstructor
@@ -21,4 +25,10 @@ public class GroupService {
         return group.getId();
     }
 
+    @Transactional(readOnly = true)
+    public Page<GroupModel.InfoWithMemberCount> getGroups(Pageable pageable) {
+        Page<GroupInfoWithMemberCount> page = groupsRepository.findGroupsWithMemberCount(pageable);
+
+        return page.map(GroupModel.InfoWithMemberCount::from);
+    }
 }
