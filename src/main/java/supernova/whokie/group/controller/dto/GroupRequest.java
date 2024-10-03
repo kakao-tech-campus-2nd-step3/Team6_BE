1540af5590e7d34796ee3e1417cf5f450fcd58b8
momnpa333 <momnpa333@gmail.com>
2024-10-03 16:56:07 +0900

    Feat: GroupRequest toCommand 제작

--- src/main/java/supernova/whokie/group/controller/dto/GroupRequest.java
+++ src/main/java/supernova/whokie/group/controller/dto/GroupRequest.java
@@ -1,13 +1,36 @@
 package supernova.whokie.group.controller.dto;
 
+import jakarta.validation.constraints.NotNull;
+import supernova.whokie.group.service.dto.GroupCommand;
+import supernova.whokie.group_member.GroupRole;
+import supernova.whokie.group_member.service.dto.GroupMemberCommand;
+
 public class GroupRequest {
 
     public record Create(
+        @NotNull
         String groupName,
+        @NotNull
         String groupDescription,
+        @NotNull
         String groupImageUrl
     ) {
 
+        public GroupCommand.Create toCommand() {
+            return GroupCommand.Create.builder()
+                .groupName(groupName)
+                .groupDescription(groupDescription)
+                .groupImageUrl(groupImageUrl)
+                .build();
+        }
+
+        public GroupMemberCommand.Create toGroupMemberCommand(Long groupId, Long userId) {
+            return GroupMemberCommand.Create.builder()
+                .role(GroupRole.LEADER)
+                .groupId(groupId)
+                .userId(userId)
+                .build();
+        }
     }
 
     public record Join(
@@ -24,9 +47,9 @@ public class GroupRequest {
 
 
     public record Modify(
-            Long groupId,
-            String groupName,
-            String description
+        Long groupId,
+        String groupName,
+        String description
     ) {
 
     }
