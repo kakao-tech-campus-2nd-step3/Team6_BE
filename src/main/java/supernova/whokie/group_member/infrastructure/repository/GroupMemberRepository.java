6fb02d7b4be39fc96d32082b36effa6a2dc5c5f1
momnpa333 <momnpa333@gmail.com>
2024-10-03 17:12:59 +0900

    Feat: existByUsersAndGroups 제작

--- src/main/java/supernova/whokie/group_member/infrastructure/repository/GroupMemberRepository.java
+++ src/main/java/supernova/whokie/group_member/infrastructure/repository/GroupMemberRepository.java
@@ -2,9 +2,12 @@ package supernova.whokie.group_member.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import supernova.whokie.group.Groups;
 import supernova.whokie.group_member.GroupMember;
+import supernova.whokie.user.Users;
 
 @Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
 
+    boolean existsByUsersAndGroups(Users user, Groups group);
 }
