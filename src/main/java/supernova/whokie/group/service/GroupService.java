efd518473ef319fa2235bed440a2e5604f26cdc6
momnpa333 <momnpa333@gmail.com>
2024-10-03 18:36:31 +0900

    Feat: modifyGroup 메서드 제작

--- src/main/java/supernova/whokie/group/service/GroupService.java
+++ src/main/java/supernova/whokie/group/service/GroupService.java
@@ -1,6 +1,5 @@
 package supernova.whokie.group.service;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,12 +10,15 @@ import supernova.whokie.group.infrastructure.repository.GroupsRepository;
 import supernova.whokie.group.infrastructure.repository.dto.GroupInfoWithMemberCount;
 import supernova.whokie.group.service.dto.GroupCommand;
 import supernova.whokie.group.service.dto.GroupModel;
+import supernova.whokie.group_member.GroupMember;
+import supernova.whokie.group_member.infrastructure.repository.GroupMemberRepository;
 
 @Service
 @RequiredArgsConstructor
 public class GroupService {
 
     private final GroupsRepository groupsRepository;
+    private final GroupMemberRepository groupMemberRepository;
 
     @Transactional
     public Long createGroup(GroupCommand.Create command) {
@@ -30,5 +32,21 @@ public class GroupService {
         Page<GroupInfoWithMemberCount> page = groupsRepository.findGroupsWithMemberCount(pageable);
 
         return page.map(GroupModel.InfoWithMemberCount::from);
+    }
+
+    @Transactional
+    public void modifyGroup(GroupCommand.Modify command, Long userId) {
+        GroupMember groupMember = groupMemberRepository.findByUsersIdAndGroupsId(userId,
+                command.groupId())
+            .orElseThrow(() -> new IllegalArgumentException("그룹에 가입되어 있지 않습니다."));
+
+        Groups group = groupsRepository.findById(command.groupId())
+            .orElseThrow(() -> new IllegalArgumentException("그룹이 존재하지 않습니다."));
+
+        if (!groupMember.isLeader()) {
+            throw new IllegalArgumentException("그룹의 리더만 그룹 정보를 수정할 수 있습니다.");
+        }
+
+        group.modify(command.groupName(), command.description());
     }
 }
