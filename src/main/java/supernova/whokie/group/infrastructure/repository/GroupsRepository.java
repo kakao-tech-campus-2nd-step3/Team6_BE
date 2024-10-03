28875707a2b7000eee48875ee8a98047f50514a6
momnpa333 <momnpa333@gmail.com>
2024-10-03 16:56:41 +0900

    Feat: GroupRepository 제작

--- src/main/java/supernova/whokie/group/infrastructure/repository/GroupsRepository.java
+++ src/main/java/supernova/whokie/group/infrastructure/repository/GroupsRepository.java
@@ -1,7 +1,8 @@
-package supernova.whokie.group.repository;
+package supernova.whokie.group.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import supernova.whokie.group.Groups;
 
 public interface GroupsRepository extends JpaRepository<Groups, Long> {
+
 }
