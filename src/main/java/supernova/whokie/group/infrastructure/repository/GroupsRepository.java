4d39aef94284776596c1d99b999fa0afae3ed0f7
momnpa333 <momnpa333@gmail.com>
2024-10-03 18:18:32 +0900

    Feat: jpql 제작

--- src/main/java/supernova/whokie/group/infrastructure/repository/GroupsRepository.java
+++ src/main/java/supernova/whokie/group/infrastructure/repository/GroupsRepository.java
@@ -1,8 +1,21 @@
 package supernova.whokie.group.infrastructure.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import supernova.whokie.group.Groups;
+import supernova.whokie.group.infrastructure.repository.dto.GroupInfoWithMemberCount;
+import supernova.whokie.group.service.dto.GroupModel;
 
 public interface GroupsRepository extends JpaRepository<Groups, Long> {
 
+    @Query(
+        "SELECT new supernova.whokie.group.infrastructure.repository.dto.GroupInfoWithMemberCount("
+            +
+            "g.id, g.groupName, g.description, g.groupImageUrl, COUNT(m.id)) " +
+            "FROM Groups g " +
+            "LEFT JOIN GroupMember m ON g.id = m.groups.id " +
+            "GROUP BY g.id")
+    Page<GroupInfoWithMemberCount> findGroupsWithMemberCount(Pageable pageable);
 }
