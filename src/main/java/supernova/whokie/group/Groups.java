f060522481eca1c903c64e1c02b9ee4aa0f5c006
momnpa333 <momnpa333@gmail.com>
2024-10-09 18:05:53 +0900

    Feat: Groups 에 제약조건 추가

--- src/main/java/supernova/whokie/group/Groups.java
+++ src/main/java/supernova/whokie/group/Groups.java
@@ -1,5 +1,6 @@
 package supernova.whokie.group;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,14 +14,19 @@ import supernova.whokie.global.entity.BaseTimeEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
-@Table(name = "`groups`")  // mysql 예약어로 인해 백틱으로 감쌈
+@Table(name = "`GROUPS`")  // mysql 예약어로 인해 백틱으로 감쌈
 public class Groups extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String groupName;
+
+    @Column(nullable = false)
     private String description;
+
+    @Column(nullable = false)
     private String groupImageUrl;
 }
