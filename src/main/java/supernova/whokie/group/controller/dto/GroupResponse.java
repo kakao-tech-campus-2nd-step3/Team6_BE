13805b863a9ea66a1e333c12b895a696744ea580
momnpa333 <momnpa333@gmail.com>
2024-10-09 19:39:24 +0900

    Feat: response 제작

--- src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
+++ src/main/java/supernova/whokie/group/controller/dto/GroupResponse.java
@@ -1,16 +1,25 @@
 package supernova.whokie.group.controller.dto;
 
 import lombok.Builder;
+import supernova.whokie.group.service.dto.GroupModel;
 
 public class GroupResponse {
 
     @Builder
     public record Info(
-            Long groupId,
-            String groupName,
-            String groupImageUrl,
-            int groupMemberCount
+        Long groupId,
+        String groupName,
+        String groupImageUrl,
+        Long groupMemberCount
     ) {
 
+        public static Info from(GroupModel.InfoWithMemberCount groupModel) {
+            return Info.builder()
+                .groupId(groupModel.groupId())
+                .groupName(groupModel.groupName())
+                .groupImageUrl(groupModel.groupImageUrl())
+                .groupMemberCount(groupModel.groupMemberCount())
+                .build();
+        }
     }
 }
