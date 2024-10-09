38839b7c0614aac5ff53f51288b4d53faca3fbb4
momnpa333 <momnpa333@gmail.com>
2024-10-09 18:33:13 +0900

    Feat: JoinGroup메서드 제작

--- src/main/java/supernova/whokie/group_member/service/GroupMemberService.java
+++ src/main/java/supernova/whokie/group_member/service/GroupMemberService.java
@@ -6,17 +6,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.global.exception.EntityNotFoundException;
 import supernova.whokie.global.exception.ForbiddenException;
+import supernova.whokie.group.Groups;
+import supernova.whokie.group.repository.GroupRepository;
 import supernova.whokie.group_member.GroupMember;
 import supernova.whokie.group_member.infrastructure.repository.GroupMemberRepository;
 import supernova.whokie.group_member.service.dto.GroupMemberCommand;
-import supernova.whokie.group_member.service.dto.GroupMemberModel.Member;
 import supernova.whokie.group_member.service.dto.GroupMemberModel.Members;
+import supernova.whokie.user.Users;
+import supernova.whokie.user.infrastructure.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class GroupMemberService {
 
     private final GroupMemberRepository groupMemberRepository;
+    private final UserRepository userRepository;
+    private final GroupRepository groupRepository;
 
     @Transactional
     public void delegateLeader(Long userId, GroupMemberCommand.Modify command) {
@@ -60,11 +65,27 @@ public class GroupMemberService {
         groupMemberRepository.deleteByUserIdAndGroupId(member.getId(), command.groupId());
     }
 
+    @Transactional(readOnly = true)
     public Members getGroupMembers(Long userId, Long groupId) {
         GroupMember member = groupMemberRepository.findByUserIdAndGroupId(userId, groupId)
             .orElseThrow(() -> new EntityNotFoundException("해당 그룹의 유저만 조회할 수 있습니다."));
 
         List<GroupMember> groupMembers = groupMemberRepository.findAllByGroupId(groupId);
         return Members.from(groupMembers);
+    }
+
+    @Transactional
+    public void joinGroup(GroupMemberCommand.Join command, Long userId) {
+        if (groupMemberRepository.existsByUserIdAndGroupId(userId, command.groupId())) {
+            throw new ForbiddenException("이미 가입한 그룹입니다.");
+        }
+        Users user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException("해당 유저가 존재하지 않습니다."));
+
+        Groups group = groupRepository.findById(command.groupId())
+            .orElseThrow(() -> new EntityNotFoundException("해당 그룹이 존재하지 않습니다."));
+
+        GroupMember groupMember = command.toEntity(user, group);
+        groupMemberRepository.save(groupMember);
     }
 }
