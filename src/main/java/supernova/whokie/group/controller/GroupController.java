925b801162497fcac0ddb1396e446f9a49618758
momnpa333 <momnpa333@gmail.com>
2024-10-09 18:05:04 +0900

    Feat: group create api 제작

--- src/main/java/supernova/whokie/group/controller/GroupController.java
+++ src/main/java/supernova/whokie/group/controller/GroupController.java
@@ -3,9 +3,11 @@ package supernova.whokie.group.controller;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+import supernova.whokie.global.annotation.Authenticate;
 import supernova.whokie.global.dto.GlobalResponse;
 import supernova.whokie.global.dto.PagingResponse;
 import supernova.whokie.group.controller.dto.GroupRequest;
@@ -13,16 +15,22 @@ import supernova.whokie.group.controller.dto.GroupResponse;
 
 import java.awt.print.Pageable;
 import java.util.List;
+import supernova.whokie.group.service.GroupService;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/group")
 public class GroupController {
 
-    @PostMapping("/")
+    private final GroupService groupService;
+
+    @PostMapping("")
     public GlobalResponse createGroup(
-        @RequestBody @Valid GroupRequest.Create request
+        @RequestBody @Valid GroupRequest.Create request,
+        @Authenticate Long userId
     ) {
-        return GlobalResponse.builder().message("dummy").build();
+        groupService.createGroup(request.toCommand(), userId);
+        return GlobalResponse.builder().message("그룹이 성공적으로 만들어졌습니다.").build();
     }
 
     @PostMapping("/join")
@@ -49,24 +57,24 @@ public class GroupController {
 
     @GetMapping("/{group-id}")
     public PagingResponse<GroupResponse.Info> getGroupPaging(
-            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
+        @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable
     ) {
         return new PagingResponse<>(
-                List.of(new GroupResponse.Info(1L, "group1", "groupImageUrl", 12),
-                        new GroupResponse.Info(2L, "group2", "groupImageUrl", 12)),
-                2, 0, 1, 1);
+            List.of(new GroupResponse.Info(1L, "group1", "groupImageUrl", 12),
+                new GroupResponse.Info(2L, "group2", "groupImageUrl", 12)),
+            2, 0, 1, 1);
     }
 
     @PatchMapping("modify")
     public GlobalResponse modifyGroup(
-            @RequestBody @Valid GroupRequest.Modify request
+        @RequestBody @Valid GroupRequest.Modify request
     ) {
         return GlobalResponse.builder().message("dummy").build();
     }
 
     @GetMapping("/{group-id}/info")
     public GroupResponse.Info getGroupInfo(
-            @PathVariable("group-id") @NotNull @Min(1) Long groupId
+        @PathVariable("group-id") @NotNull @Min(1) Long groupId
     ) {
         return new GroupResponse.Info(groupId, "group1", "groupImageUrl", 12);
     }
