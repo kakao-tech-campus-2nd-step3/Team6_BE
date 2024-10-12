ac24818700a5899b9eb4dd2456c74620a3776e47
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-10-12 23:31:30 +0900

    Feat: 프로필 서비스 테스트 코드 수정

--- src/test/java/supernova/whokie/profile/ProfileIntegrationTest.java
+++ src/test/java/supernova/whokie/profile/ProfileIntegrationTest.java
@@ -10,14 +10,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import supernova.whokie.global.auth.JwtProvider;
 import supernova.whokie.profile.infrastructure.ProfileRepository;
 import supernova.whokie.user.Gender;
 import supernova.whokie.user.Role;
