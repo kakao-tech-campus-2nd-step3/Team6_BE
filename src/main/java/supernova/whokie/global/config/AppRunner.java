7be5cb3c7600c6bf1f8698898df5a310c7297abb
momnpa333 <momnpa333@gmail.com>
2024-10-13 14:09:19 +0900

    Refact: log로 변경

--- src/main/java/supernova/whokie/global/config/AppRunner.java
+++ src/main/java/supernova/whokie/global/config/AppRunner.java
@@ -1,11 +1,13 @@
 package supernova.whokie.global.config;
 
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class AppRunner implements ApplicationRunner {
 
@@ -17,12 +19,10 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("===================활성화된 프로필===================");
-        System.out.println("Active profiles : " + Arrays.toString(environment.getActiveProfiles()));
-        System.out.println("db url : " + environment.getProperty("spring.datasource.url"));
-        System.out.println(
-            "db username : " + environment.getProperty("spring.datasource.username"));
-        System.out.println("secret key : " + environment.getProperty("jwt.secret"));
-
+        log.info("=================== 활성화된 프로필 ===================");
+        log.info("Active profiles : {}", Arrays.toString(environment.getActiveProfiles()));
+        log.info("db url : {}", environment.getProperty("spring.datasource.url"));
+        log.info("db username : {}", environment.getProperty("spring.datasource.username"));
+        log.info("secret key : {}", environment.getProperty("jwt.secret"));
     }
 }
