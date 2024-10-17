bfa7702f75cd2788bc40f2e5da6be11785dde401
momnpa333 <momnpa333@gmail.com>
2024-10-17 13:32:54 +0900

    Refact: FriendReaderService 업데이트

--- src/main/java/supernova/whokie/friend/service/FriendReaderService.java
+++ src/main/java/supernova/whokie/friend/service/FriendReaderService.java
@@ -1,8 +1,11 @@
 package supernova.whokie.friend.service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.friend.Friend;
 import supernova.whokie.friend.infrastructure.repository.FriendRepository;
 import supernova.whokie.user.Users;
@@ -15,5 +18,21 @@ public class FriendReaderService {
 
     public List<Friend> getAllByHostUser(Users user) {
         return friendRepository.findAllByHostUser(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Set<Long> getFriendIdsByHostUser(Long userId) {
+        List<Friend> existingList = friendRepository.findByHostUserIdFetchJoin(userId);
+        return extractFriendUserIdAsSet(existingList);
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<Friend> findByHostUserIdFetchJoin(Long userId) {
+        return friendRepository.findByHostUserIdFetchJoin(userId);
+    }
+
+    public Set<Long> extractFriendUserIdAsSet(List<Friend> friends) {
+        return friends.stream().map(Friend::getFriendUserId).collect(Collectors.toSet());
     }
 }
