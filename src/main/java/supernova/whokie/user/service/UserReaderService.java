ced9951fa89cdfbf08f0d8865108216133e1a971
momnpa333 <momnpa333@gmail.com>
2024-10-17 13:33:26 +0900

    Refact: UserReaderService 리팩토링

--- src/main/java/supernova/whokie/user/service/UserReaderService.java
+++ src/main/java/supernova/whokie/user/service/UserReaderService.java
@@ -1,5 +1,6 @@
 package supernova.whokie.user.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,7 +8,6 @@ import supernova.whokie.global.constants.MessageConstants;
 import supernova.whokie.global.exception.EntityNotFoundException;
 import supernova.whokie.user.Users;
 import supernova.whokie.user.infrastructure.repository.UserRepository;
-import supernova.whokie.user.service.dto.UserModel;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +26,16 @@ public class UserReaderService {
     @Transactional(readOnly = true)
     public boolean isUserExist(Long userId) {
         return userRepository.existsById(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Users> getUserListByKakaoIdIn(List<Long> kakaoId) {
+        return userRepository.findByKakaoIdIn(kakaoId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Users> getUserListByUserIdIn(List<Long> userId) {
+        return userRepository.findByIdIn(userId);
     }
 
 }
