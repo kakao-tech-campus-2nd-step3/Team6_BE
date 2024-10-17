654701061d80f18643fb812615c0773f077d98df
momnpa333 <momnpa333@gmail.com>
2024-10-17 14:08:46 +0900

    Refact: GroupMemberReaderService 리팩토링

--- src/main/java/supernova/whokie/group_member/service/GroupMemberReaderService.java
+++ src/main/java/supernova/whokie/group_member/service/GroupMemberReaderService.java
@@ -1,5 +1,6 @@
 package supernova.whokie.group_member.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +8,7 @@ import supernova.whokie.global.constants.MessageConstants;
 import supernova.whokie.global.exception.EntityNotFoundException;
 import supernova.whokie.group_member.GroupMember;
 import supernova.whokie.group_member.infrastructure.repository.GroupMemberRepository;
+import supernova.whokie.group_member.service.dto.GroupMemberModel.Members;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +24,19 @@ public class GroupMemberReaderService {
                 () -> new EntityNotFoundException(MessageConstants.GROUP_MEMBER_NOT_FOUND_MESSAGE));
     }
 
+    @Transactional(readOnly = true)
+    public boolean isGroupMemberExist(Long userId, Long groupId) {
+        return groupMemberRepository.existsByUserIdAndGroupId(userId, groupId);
+    }
+
+    @Transactional(readOnly = true)
+    public Members getGroupMembers(Long userId, Long groupId) {
+        if (!groupMemberRepository.existsByUserIdAndGroupId(userId, groupId)) {
+            throw new EntityNotFoundException(MessageConstants.GROUP_MEMBER_NOT_FOUND_MESSAGE);
+        }
+
+        List<GroupMember> groupMembers = groupMemberRepository.findAllByGroupId(groupId);
+        return Members.from(groupMembers);
+    }
 
 }
