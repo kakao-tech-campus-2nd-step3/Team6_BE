f8814652af51f513821f23bbc79d4e920904d5e6
momnpa333 <momnpa333@gmail.com>
2024-10-17 13:44:13 +0900

    Refact: GroupReaderService 리팩토링

--- src/main/java/supernova/whokie/group/service/GroupReaderService.java
+++ src/main/java/supernova/whokie/group/service/GroupReaderService.java
@@ -1,11 +1,16 @@
 package supernova.whokie.group.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.global.constants.MessageConstants;
 import supernova.whokie.global.exception.EntityNotFoundException;
 import supernova.whokie.group.Groups;
 import supernova.whokie.group.repository.GroupRepository;
+import supernova.whokie.group.repository.dto.GroupInfoWithMemberCount;
+import supernova.whokie.group.service.dto.GroupModel.InfoWithMemberCount;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +21,21 @@ public class GroupReaderService {
     public Groups getGroupById(Long groupId) {
         return groupRepository.findById(groupId).orElseThrow(() -> new EntityNotFoundException(
             MessageConstants.GROUP_NOT_FOUND_MESSAGE));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<InfoWithMemberCount> getGroupPaging(Long userId, Pageable pageable) {
+        Page<GroupInfoWithMemberCount> groupPage = groupRepository.findGroupsWithMemberCountByUserId(
+            userId,
+            pageable);
+        return groupPage.map(InfoWithMemberCount::from);
+    }
+
+    @Transactional(readOnly = true)
+    public GroupInfoWithMemberCount getGroupInfoWithMemberCountByGroupId(Long groupId) {
+        return groupRepository.findGroupInfoWithMemberCountByGroupId(groupId)
+            .orElseThrow(
+                () -> new EntityNotFoundException(MessageConstants.GROUP_NOT_FOUND_MESSAGE));
     }
 
 }
