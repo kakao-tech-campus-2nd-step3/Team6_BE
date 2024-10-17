88c60c449043c3fc0541adf20dcadd13e2285226
momnpa333 <momnpa333@gmail.com>
2024-10-17 14:08:33 +0900

    Refact: GroupMemberController 리팩토링

--- src/main/java/supernova/whokie/group_member/controller/GroupMemberController.java
+++ src/main/java/supernova/whokie/group_member/controller/GroupMemberController.java
@@ -15,7 +15,9 @@ import supernova.whokie.global.annotation.Authenticate;
 import supernova.whokie.global.dto.GlobalResponse;
 import supernova.whokie.group_member.controller.dto.GroupMemberRequest;
 import supernova.whokie.group_member.controller.dto.GroupMemberResponse;
+import supernova.whokie.group_member.service.GroupMemberReaderService;
 import supernova.whokie.group_member.service.GroupMemberService;
+import supernova.whokie.group_member.service.GroupMemberWriterService;
 import supernova.whokie.group_member.service.dto.GroupMemberModel;
 
 @RestController
@@ -23,12 +25,14 @@ import supernova.whokie.group_member.service.dto.GroupMemberModel;
 @RequestMapping("/api/group")
 public class GroupMemberController {
 
+    private final GroupMemberReaderService groupMemberReaderService;
+    private final GroupMemberWriterService groupMemberWriterService;
     private final GroupMemberService groupMemberService;
 
     @PatchMapping("leader")
     public GlobalResponse updateGroupLeader(
-            @RequestBody @Valid GroupMemberRequest.Modify request,
-            @Authenticate Long userId
+        @RequestBody @Valid GroupMemberRequest.Modify request,
+        @Authenticate Long userId
     ) {
         groupMemberService.delegateLeader(userId, request.toCommand());
         return GlobalResponse.builder().message("그룹장 위임에 성공하였습니다.").build();
@@ -36,26 +40,26 @@ public class GroupMemberController {
 
     @PostMapping("/expel")
     public GlobalResponse expelGroupMember(
-            @RequestBody @Valid GroupMemberRequest.Expel request,
-            @Authenticate Long userId
+        @RequestBody @Valid GroupMemberRequest.Expel request,
+        @Authenticate Long userId
     ) {
-        groupMemberService.expelMember(userId, request.toCommand());
+        groupMemberWriterService.expelMember(userId, request.toCommand());
         return GlobalResponse.builder().message("그룹 멤버 강퇴에 성공하였습니다.").build();
     }
 
     @GetMapping("/{group-id}/member")
     public GroupMemberResponse.Members getGroupMemberList(
-            @PathVariable("group-id") @NotNull @Min(1) Long groupId,
-            @Authenticate Long userId
+        @PathVariable("group-id") @NotNull @Min(1) Long groupId,
+        @Authenticate Long userId
     ) {
-        GroupMemberModel.Members model = groupMemberService.getGroupMembers(userId, groupId);
+        GroupMemberModel.Members model = groupMemberReaderService.getGroupMembers(userId, groupId);
         return GroupMemberResponse.Members.from(model);
     }
 
     @PostMapping("/join")
     public GlobalResponse joinGroup(
-            @RequestBody @Valid GroupMemberRequest.Join request,
-            @Authenticate Long userId
+        @RequestBody @Valid GroupMemberRequest.Join request,
+        @Authenticate Long userId
     ) {
         groupMemberService.joinGroup(request.toCommand(), userId);
         return GlobalResponse.builder().message("그룹 가입에 성공했습니다.").build();
@@ -63,11 +67,11 @@ public class GroupMemberController {
 
     @PostMapping("/exit")
     public GlobalResponse exitGroup(
-            @RequestBody @Valid GroupMemberRequest.Exit request,
-            @Authenticate Long userId
+        @RequestBody @Valid GroupMemberRequest.Exit request,
+        @Authenticate Long userId
     ) {
 
-        groupMemberService.exitGroup(request.toCommand(), userId);
+        groupMemberWriterService.exitGroup(request.toCommand(), userId);
         return GlobalResponse.builder().message("dummy").build();
     }
 }
