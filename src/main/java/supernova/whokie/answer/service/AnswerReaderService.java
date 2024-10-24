f331dd3df105d86b74e9c914bbbbe2a31fae03d0
momnpa333 <momnpa333@gmail.com>
2024-10-24 17:21:04 +0900

    Feat: transactional 추가

--- src/main/java/supernova/whokie/answer/service/AnswerReaderService.java
+++ src/main/java/supernova/whokie/answer/service/AnswerReaderService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import supernova.whokie.answer.Answer;
 import supernova.whokie.answer.repository.AnswerRepository;
 import supernova.whokie.global.constants.MessageConstants;
@@ -17,12 +18,14 @@ public class AnswerReaderService {
 
     private final AnswerRepository answerRepository;
 
+    @Transactional(readOnly = true)
     public Page<Answer> getAnswerList(Pageable pageable, Users user, LocalDateTime startDate,
         LocalDateTime endDate) {
         return answerRepository.findAllByPickerAndCreatedAtBetween(pageable, user, startDate,
             endDate);
     }
 
+    @Transactional(readOnly = true)
     public Answer getAnswerById(Long answerId) {
         return answerRepository.findById(answerId).orElseThrow(() -> new EntityNotFoundException(
             MessageConstants.ANSWER_NOT_FOUND_MESSAGE));
