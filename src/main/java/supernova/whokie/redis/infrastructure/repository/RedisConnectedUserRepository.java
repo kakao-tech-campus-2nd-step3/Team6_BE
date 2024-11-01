96acea78e1b690d5e5bb8db282df6935ed237317
hjinshin <gudwls818@gmail.com>
2024-11-02 02:23:34 +0900

    Fix: Redis에 SseEmitter를 저장하지 않도록 변경

--- src/main/java/supernova/whokie/redis/infrastructure/repository/RedisConnectedUserRepository.java
+++
@@ -1,7 +0,0 @@
-package supernova.whokie.redis.infrastructure.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import supernova.whokie.redis.entity.ConnectedUser;
-
-public interface RedisConnectedUserRepository extends CrudRepository<ConnectedUser, Long> {
-}
