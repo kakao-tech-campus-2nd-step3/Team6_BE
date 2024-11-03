0568a38bdc6032d76c3a8d8b64a838ca05e8d562
geonny <kky8586696@naver.com>
2024-11-03 23:26:18 +0900

    Refactor: 주석 추가

--- src/main/java/supernova/whokie/answer/service/AnswerService.java
+++ src/main/java/supernova/whokie/answer/service/AnswerService.java
@@ -153,10 +153,10 @@ public class AnswerService {
         List<AnswerModel.Hint> allHints = new ArrayList<>();
 
         Users picker = answer.getPicker();
-        String parsedINameInitial = getInitials(picker.getName());
+        String parsedNameInitial = getInitials(picker.getName()); //이름 초성으로 변환
         for (int i = 1; i <= Constants.MAX_HINT_COUNT; i++) {
             boolean valid = (i <= answer.getHintCount());
-            allHints.add(AnswerModel.Hint.from(picker, i, valid, parsedINameInitial));
+            allHints.add(AnswerModel.Hint.from(picker, i, valid, parsedNameInitial));
         }
 
         return allHints;
